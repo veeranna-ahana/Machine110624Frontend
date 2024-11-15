74a1920317e29a67f82aa9c0a7237c027afa130c
veeranna-ahana <bedasur.veeranna@ahana.co.in>
2024-11-15 14:37:15 +0530

    15-11-2024

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -6,27 +6,26 @@ import * as MdIcon from "react-icons/md";
 import { VscTypeHierarchySub } from "react-icons/vsc";
 import { BiFoodMenu } from "react-icons/bi";
 import { HiUsers } from "react-icons/hi";
-import { BsPersonFill,BsFillGearFill,BsScrewdriver } from "react-icons/bs";
+import { BsPersonFill, BsFillGearFill, BsScrewdriver } from "react-icons/bs";
 import { AiFillCreditCard } from "react-icons/ai";
 import { DiOpenshift } from "react-icons/di";
-import {MdReport} from "react-icons/md";
-import {MdHomeRepairService} from "react-icons/md"
-import {BsListCheck} from "react-icons/bs";
-import {BiGitMerge} from "react-icons/bi";
-import {SiGoogletagmanager, SiRedhatopenshift} from "react-icons/si";
-import {BsServer} from "react-icons/bs";
-import {FiCpu} from "react-icons/fi"
-import {VscServerProcess} from "react-icons/vsc"
-import {FiGitPullRequest} from "react-icons/fi"
-import {AiOutlineOrderedList} from "react-icons/ai"
-import {FiEdit} from "react-icons/fi"
-import {FaDropbox} from "react-icons/fa";
-import {GoReport} from "react-icons/go"
-import {AiOutlineSchedule} from "react-icons/ai"
-import {GiLaserPrecision} from "react-icons/gi"
-import {HiCubeTransparent} from "react-icons/hi"
-import {AiFillSchedule} from "react-icons/ai"
-
+import { MdReport } from "react-icons/md";
+import { MdHomeRepairService } from "react-icons/md";
+import { BsListCheck } from "react-icons/bs";
+import { BiGitMerge } from "react-icons/bi";
+import { SiGoogletagmanager, SiRedhatopenshift } from "react-icons/si";
+import { BsServer } from "react-icons/bs";
+import { FiCpu } from "react-icons/fi";
+import { VscServerProcess } from "react-icons/vsc";
+import { FiGitPullRequest } from "react-icons/fi";
+import { AiOutlineOrderedList } from "react-icons/ai";
+import { FiEdit } from "react-icons/fi";
+import { FaDropbox } from "react-icons/fa";
+import { GoReport } from "react-icons/go";
+import { AiOutlineSchedule } from "react-icons/ai";
+import { GiLaserPrecision } from "react-icons/gi";
+import { HiCubeTransparent } from "react-icons/hi";
+import { AiFillSchedule } from "react-icons/ai";
 
 export const customerSidebar = [
   {
@@ -39,16 +38,25 @@ export const customerSidebar = [
       {
         title: "Machine Operator",
         // path: "/production/setup/server",
-        path:"/Machine/MachineOperator",
+        path: "/Machine/MachineOperator",
         icon: <BsServer />,
       },
       {
         title: "Print Lable",
         // path: "/production/setup/process",
-        path:"/Machine/PrintLabel",
+        path: "/Machine/PrintLabel",
         icon: <VscServerProcess />,
       },
     ],
+  },
+  {
+    title: "Previous Menu",
+    // path: "http://172.16.20.61:3000/home",
+    path: "http://192.168.1.25:3000/home",
+    // path: "http://192.168.1.25:9000/home",
+    icon: <MdIcon.MdPreview />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
   },
 
   // {
@@ -66,15 +74,15 @@ export const customerSidebar = [
   //         {
   //           title: "Profile",
   //           // path: "/production/production/schedulelistprofile",
-  //           icon: <AiIcons.AiOutlineArrowRight />            
+  //           icon: <AiIcons.AiOutlineArrowRight />
   //         },{
   //           title: "Fabrication",
   //           // path: "/production/production/schedulelistfabrication",
-  //           icon: <AiIcons.AiOutlineArrowRight />            
+  //           icon: <AiIcons.AiOutlineArrowRight />
   //         },{
   //           title: "Services",
   //           // path: "/production/production/schedulelistservice",
-  //           icon: <AiIcons.AiOutlineArrowRight />            
+  //           icon: <AiIcons.AiOutlineArrowRight />
   //         }]
   //     },
   //     {
@@ -85,15 +93,14 @@ export const customerSidebar = [
   //         {
   //           title: "Profile",
   //           // path: "/production/production/machineallotmentProfile",
-  //           icon: <AiIcons.AiOutlineArrowRight />            
+  //           icon: <AiIcons.AiOutlineArrowRight />
   //         },
   //         {
   //           title: "Service",
   //           // path: "/production/production/machineallotmentservice",
-  //           icon: <AiIcons.AiOutlineArrowRight />            
+  //           icon: <AiIcons.AiOutlineArrowRight />
   //         }
   //       ]
-
 
   //     },
   //     {
@@ -104,15 +111,15 @@ export const customerSidebar = [
   //         {
   //           title: "Profile",
   //           // path: "/production/production/shiftmanagerProfile",
-  //           icon: <AiIcons.AiOutlineArrowRight />            
+  //           icon: <AiIcons.AiOutlineArrowRight />
   //         },{
   //           title: "Fabrication",
   //           // path: "/production/production/shiftmanagerFabrication",
-  //           icon: <AiIcons.AiOutlineArrowRight />            
+  //           icon: <AiIcons.AiOutlineArrowRight />
   //         },{
   //           title: "Services",
   //           // path: "/production/production/shiftmanagerService",
-  //           icon: <AiIcons.AiOutlineArrowRight />            
+  //           icon: <AiIcons.AiOutlineArrowRight />
   //         }]
   //     },
   //   ]
@@ -216,7 +223,6 @@ export const customerSidebar = [
   //     },
   //   ]
   // },
-  
 ];
 
 export const adminSidebar = [
